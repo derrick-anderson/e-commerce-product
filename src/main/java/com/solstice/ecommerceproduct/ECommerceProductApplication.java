da77e1e3281794e29bd52e33dad524248a7579b8
Derrick Anderson <deanderson@solstice.com>
2018-09-16 23:55:31 -0500

    Initial Commit

---
+++ src/main/java/com/solstice/ecommerceproduct/ECommerceProductApplication.java
@@ -0,0 +1,12 @@
+package com.solstice.ecommerceproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceProductApplication.class, args);
+    }
+}
